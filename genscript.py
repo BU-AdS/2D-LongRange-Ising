c9a9aeddf936de67ce719c5c28d6c4100631660a
Lorenzo Vitale <lorenzovitale89@gmail.com>
2019-02-14 16:24:47 -0500

    updated script generating files

--- genscript.py
+++ genscript.py
@@ -8,39 +8,35 @@ execute = True
 maxhours = 120
 maxmem = 125
 
-# Critical temperature for infinite volume system
-J0 = 0.4406867935
-
-Jlist = np.linspace(0.9, 1.1, 11)*J0
-# Jlist = np.linspace(1, 1, 1)*J0
 
 for J in Jlist:
-    dname = "J={}".format(J)
-    fname = "template_ising.sh"
+    for side in sideList:
+        dname = dirname(J, side)
+        fname = "template_ising.sh"
 
-    scriptname = "{}/script.sh".format(dname)
-    f = open(scriptname,'wt')
+        scriptname = "{}/script.sh".format(dname)
+        f = open(scriptname,'wt')
 
-    jobname = "J={}".format(J)
+        jobname = dname
 
-    f.write("#!/bin/bash -l\n\n")
-    f.write("#$ -l h_rt={}:00:00\n".format(maxhours))
-    f.write("#$ -l mem_total={}G\n".format(maxmem))
-    f.write("#$ -m a\n")
-    f.write("#$ -j y\n")
-    #f.write("#$ -pe omp 2\n")
-    f.write("#$ -N {}\n\n".format(jobname))
+        f.write("#!/bin/bash -l\n\n")
+        f.write("#$ -l h_rt={}:00:00\n".format(maxhours))
+        f.write("#$ -l mem_total={}G\n".format(maxmem))
+        f.write("#$ -m a\n")
+        f.write("#$ -j y\n")
+        #f.write("#$ -pe omp 2\n")
+        f.write("#$ -N {}\n\n".format(jobname))
 
-    f.write("module load gsl\n")
-    f.write("module load gcc/7.2.0\n")
+        f.write("module load gsl\n")
+        f.write("module load gcc/7.2.0\n")
 
-    # f.write("{}/{}\n\n".format(dname,fname))
-    f.write("./{} 1>/dev/null\n\n".format(fname))
-    f.close()
+        # f.write("{}/{}\n\n".format(dname,fname))
+        f.write("./{} 1>/dev/null\n\n".format(fname))
+        f.close()
 
-    os.chmod(scriptname, 0o755)
+        os.chmod(scriptname, 0o755)
 
-    if execute:
-	    os.chdir(dname)
-	    subprocess.run(["qsub", "script.sh"])
-	    os.chdir('..')
+        if execute:
+                os.chdir(dname)
+                subprocess.run(["qsub", "script.sh"])
+                os.chdir('..')
--- gentemplates.py
+++ gentemplates.py
@@ -2,59 +2,55 @@ import sys
 import os
 import numpy as np
 from shutil import copyfile
+from util import *
 
 execute = False
 
 nthreads = 1
 
-# Critical temperature for infinite volume system
-J0 = 0.4406867935
-
-Jlist = np.linspace(0.9, 1.1, 11)*J0
-# Jlist = np.linspace(1, 1, 1)*J0
-
 print(Jlist)
 
 for J in Jlist:
-    dname = "J={}".format(J)
+    for side in sideList:
+        dname = dirname(J, side)
 
-    if not os.path.exists(dname):
-        os.makedirs(dname)
+        if not os.path.exists(dname):
+            os.makedirs(dname)
 
-    execname = "long_range"
-    copyfile(execname, "{}/{}".format(dname,execname))
-    os.chmod("{}/{}".format(dname,execname), 0o755)
+        execname = "long_range"
+        copyfile(execname, "{}/{}".format(dname,execname))
+        os.chmod("{}/{}".format(dname,execname), 0o755)
 
-    fname = "{}/template_ising.sh".format(dname)
-    sys.stdout = open(fname,'wt')
+        fname = "{}/template_ising.sh".format(dname)
+        sys.stdout = open(fname,'wt')
 
-    print(
-    "#!/bin/bash\n"\
-    "export OMP_NUM_THREADS="+str(nthreads)+"\n"\
-    "VERBOSITY='q'\n"
-    "THEORY='ISING'\n"\
-    "CLUSTER='WOLFF'\n"\
-    "CLUSTER_ARCH='CPU'\n"\
-    "COUPLING_TYPE='SR'\n"+\
-    "J={}\n".format(J)+\
-    "H=0.0\n"\
-    "SIGMA=1.5\n"\
-    "CIRCUMFERENCE=64\n"\
-    "TIMESLICES=64\n"\
-    "N_THERM=500\n"\
-    "N_MEAS=10000\n"\
-    "N_SKIP=50\n"\
-    "N_JKBLK=100\n"\
-    "N_WRITE=100\n"\
-    "COMMAND=\"./"+execname+\
-    " --verbosity ${VERBOSITY} --theory ${THEORY} --couplingType ${COUPLING_TYPE} --J ${J} --h ${H} --Lt ${TIMESLICES} --S1 ${CIRCUMFERENCE} --nTherm ${N_THERM} --nMeas ${N_MEAS} --nWrite ${N_WRITE} --nJkBlock ${N_JKBLK} --nSkip ${N_SKIP} --sigma ${SIGMA} --clusterAlg ${CLUSTER} --clusterArch ${CLUSTER_ARCH} \"\n"\
-    "echo \"\"\n"\
-    "echo \"Command given:\"\n"\
-    "echo ${COMMAND}\n"\
-    "echo \"\"\n"\
-    "${COMMAND}\n")
+        print(
+        "#!/bin/bash\n"\
+        "export OMP_NUM_THREADS="+str(nthreads)+"\n"\
+        "VERBOSITY='q'\n"
+        "THEORY='ISING'\n"\
+        "CLUSTER='WOLFF'\n"\
+        "CLUSTER_ARCH='CPU'\n"\
+        "COUPLING_TYPE='SR'\n"+\
+        "J={}\n".format(J)+\
+        "H=0.0\n"\
+        "SIGMA=1.5\n"\
+        "CIRCUMFERENCE={}\n".format(side)+\
+        "TIMESLICES={}\n".format(side)+\
+        "N_THERM=500\n"\
+        "N_MEAS=10000\n"\
+        "N_SKIP=50\n"\
+        "N_JKBLK=100\n"\
+        "N_WRITE=100\n"\
+        "COMMAND=\"./"+execname+\
+        " --verbosity ${VERBOSITY} --theory ${THEORY} --couplingType ${COUPLING_TYPE} --J ${J} --h ${H} --Lt ${TIMESLICES} --S1 ${CIRCUMFERENCE} --nTherm ${N_THERM} --nMeas ${N_MEAS} --nWrite ${N_WRITE} --nJkBlock ${N_JKBLK} --nSkip ${N_SKIP} --sigma ${SIGMA} --clusterAlg ${CLUSTER} --clusterArch ${CLUSTER_ARCH} \"\n"\
+        "echo \"\"\n"\
+        "echo \"Command given:\"\n"\
+        "echo ${COMMAND}\n"\
+        "echo \"\"\n"\
+        "${COMMAND}\n")
 
-    os.chmod(fname, 0o755)
+        os.chmod(fname, 0o755)
 
 
     sys.stdout.close()
