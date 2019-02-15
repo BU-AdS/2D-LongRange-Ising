52f6647aa43b762b9a93e74b99421a36c142e9e3
Lorenzo Vitale <lorenzovitale89@gmail.com>
2019-02-15 13:46:11 -0500

    improved plots, and subtracted average magnetization from correlators

--- plotter.py
+++ plotter.py
@@ -2,32 +2,54 @@ from matplotlib import pyplot as plt
 import numpy as np
 from numpy import array, log, pi, exp, sqrt, e
 from scipy.optimize import curve_fit
-from sys import exit
+from sys import exit, argv
 from util import *
 import os
+import sys
 
-def fitfun(x, a, b, c):
-    return a*x**(-b)+c
+form = "png"
 
 fname = "correlators_dth0.dat"
+fname_mag = "JKobservables.dat"
 miny = 1
 
+if len(sys.argv)<2:
+    print("{} <C>".format(argv[0]))
+    exit(0)
+
+C = int(argv[1])
+
+
 for J in Jlist[::2]:
-    dname = "J={:.8f}".format(J)
+    dname = "J={:.8f}_C={}_v2".format(J,C)
     os.chdir(dname)
+
 
     with open(fname, 'r') as f:
         corr = array([float(x.split()[1]) for x in f.readlines()])
         x = range(len(corr))
 
-        plt.loglog(x, corr, linestyle='--', marker='o', label=dname)
-        miny = min(miny, min(corr))
+    with open(fname_mag, 'r') as f:
+        # Read last line from file, corresponding to the most accurate estimator
+        for line in f:
+            pass
+        last = line
+        # Magnetization is third to last object according to data_io.cpp
+        mag = float(last.split()[-3])
+
+    y = corr-mag**2
+    plt.loglog(x, y, linestyle='--', marker='o', label=dname)
+    plt.loglog(x, corr)
+    miny = min(miny, min(y))
 
     os.chdir('..')
 
+
 plt.xlabel("r")
-plt.ylabel("<s(r) s(0)>")
+plt.ylabel("<s(r) s(0)>-<s>^2")
 plt.xlim(min(x)-0.1,max(x)+0.1)
-plt.ylim(miny-0.1, 1+0.1)
+# plt.ylim(miny-0.1, 1+0.1)
+plt.ylim(0.1, 1+0.1)
 plt.legend(loc=3)
-plt.savefig("corr.pdf")
+plt.title("C={}".format(C))
+plt.savefig("corr_C={}.{}".format(C,form))
--- util.py
+++ util.py
@@ -3,8 +3,8 @@ import numpy as np
 # Critical temperature for infinite volume system
 J0 = 0.4406867935
 
-Jlist = np.linspace(0.8, 1.2, 21)*J0
-Jlist = np.linspace(0.9, 1.1, 11)*J0
+Jlist = np.linspace(0.8, 1.2, 11)*J0
+# Jlist = np.linspace(0.9, 1.1, 11)*J0
 
 # Side lenght of square box
 sideList = [8,16,32,64,128]
