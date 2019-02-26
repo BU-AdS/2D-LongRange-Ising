7cf1a645690e8ad7f4025eaf284c939fcd334f5d
Lorenzo Vitale <lorenzovital89@gmail.com>
2019-02-26 10:34:06 -0500

    plotting corr functions around finite volume critical values

---
+++ plotterCrit.py
@@ -0,0 +1,84 @@
+#################
+# Plot the correlation function around the finite volume critical temperature T_C(L)
+################
+
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy import array, log, pi, exp, sqrt, e
+from scipy.optimize import curve_fit
+from sys import exit, argv
+from util import *
+import os
+import sys
+import pandas as pd
+from paramplots import *
+
+form = "png"
+
+fname = "correlators_dth0.dat"
+fname_mag = "JKobservables.csv"
+miny = 1
+
+if len(sys.argv)<2:
+    print("{} <C>".format(argv[0]))
+    exit(0)
+
+C = int(argv[1])
+
+
+for i,J in enumerate(JcritDict[C]):
+    dname = dirname(J,C)
+    os.chdir(dname)
+
+    with open(fname, 'r') as f:
+        corr = array([float(x.split()[1]) for x in f.readlines()])
+        x = range(len(corr))
+
+    data = pd.read_csv(fname_mag)
+    # Time series of total magnetization
+    magseries = data['avePhi'].tolist()
+    # magseries = data['avePhiAb'].tolist()
+    # Integrated time series of total magnetization
+    intmagseries = np.cumsum(magseries) / np.array(range(1,len(magseries)+1))
+    # Statistical average of the total magnetization over the time series.
+    # XXX To get rid of initial condition we should probably discard the initial data
+    mag = intmagseries[-1]
+
+    # Subtract disconnected component
+    y = corr-mag**2
+
+    print("J={}".format(J))
+    print("corr={}".format(corr))
+    print("y={}".format(y))
+
+    c = next(color_cycle)
+    plt.loglog(x, y, linestyle='', marker='o', label=r'J={:.8f}'.format(J), c=c)
+    plt.loglog(x, corr, c=c)
+    miny = min(miny, min(y))
+
+    if i==1:
+        print("J={} selected for fit".format(J))
+        xx = range(1,10)
+        y = [corr[xi]-mag**2 for xi in xx]
+        xx = array(xx)
+        coef = np.polyfit(log(xx), log(y), 1)
+        print("Power of the fit: {:.8f}".format(coef[0]))
+        xfit = np.linspace(0,max(log(x)),100)
+        plt.loglog(exp(xfit), exp(coef[1]+coef[0]*xfit), label="fit")
+        #, label={r"$C*x^{{{:.4f}}}$".format(coef[0])})
+        # plt.text(x=2, y=1, s=r"$x^{{{:.4f}}}$".format(coef[0]), fontsize=14)
+
+    # plt.plot(x, log(y), linestyle='', marker='o', label=r'J={:.8f}'.format(J), c=c)
+    # miny = min(miny, min(log(y)))
+
+    os.chdir('..')
+
+
+plt.xlabel(r"r")
+plt.ylabel(r"$\langle s(r) s(0)\rangle -\langle s\rangle^2$")
+plt.xlim(min(x)-0.1,max(x)+0.1)
+# plt.ylim(miny-0.1, 1+0.1)
+# plt.ylim(0.1, 1+0.1)
+plt.legend(loc=3)
+plt.title("C={}, pow={:.4f}".format(C, coef[0]))
+plt.savefig("corr_C={}.{}".format(C,form))
